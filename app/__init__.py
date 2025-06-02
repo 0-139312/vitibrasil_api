40b9569437be38cc5974ed17750bd35d26201dcf
Alexandre Mazzini <alexandreqmazzini@gmail.com>
2025-06-02 17:32:52 -0300

    Corrige config do Swagger com chave 'specs'

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,21 @@ from app.routes.exportacao import exportacao_bp
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-    Swagger(app, config={"specs_route": "/apidocs"})
+    Swagger(app, config={
+        "headers": [],
+        "specs": [
+            {
+                "endpoint": 'apispec_1',
+                "route": '/apispec_1.json',
+                "rule_filter": lambda rule: True,  # todas as rotas
+                "model_filter": lambda tag: True,  # todos os modelos
+            }
+        ],
+        "static_url_path": "/flasgger_static",
+        "swagger_ui": True,
+        "specs_route": "/apidocs" 
+    })
+
 
     app.register_blueprint(producao_bp)
     app.register_blueprint(processamento_bp)
